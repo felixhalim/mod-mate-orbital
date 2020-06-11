5501aa809d27cdd0a3a41d809743aaa71cf0b76d
Felix Halim <felix.halim@outlook.com>
2020-06-11 18:05:04 +0800

    fix: mobile responsiveness

--- src/pages/login.pages.jsx
+++ src/pages/login.pages.jsx
@@ -6,7 +6,10 @@ import WelcomeContent from "../components/welcomecontent.component";
 const Login = () => {
   return (
     <Grid container style={{ height: "100vh" }}>
-      <Grid container item xs={6}>
+      <Grid container item xs={12} sm={6}>
+        <WelcomeContent></WelcomeContent>
+      </Grid>
+      <Grid container item xs={12} sm={6}>
         <Grid item xs={2} />
         <Grid container item xs={8}>
           <Grid item xs={12}>
@@ -14,15 +17,12 @@ const Login = () => {
           </Grid>
           <Grid item xs={12}>
             <Typography variant="body2" align="center">
-              Copyright ©<Link href="#"> ModMate </Link>
+              Copyright ©<Link href="/"> ModMate </Link>
               {new Date().getFullYear()}
             </Typography>
           </Grid>
         </Grid>
         <Grid item xs={2} />
-      </Grid>
-      <Grid container item xs={6}>
-        <WelcomeContent></WelcomeContent>
       </Grid>
     </Grid>
   );
