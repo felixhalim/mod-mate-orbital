926f8de2946884f1fe2dc1c88c824db710105fdb
Felix Halim <felix.halim@outlook.com>
2020-06-21 15:38:16 +0800

    config: wrap App in UserProvider for UserContext purpose

--- src/App.js
+++ src/App.js
@@ -3,27 +3,30 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/login.pages";
 import SignUp from "./pages/signup.pages";
 // import TestPage from "./pages/testpage.pages";
+import { UserProvider } from "./context/UserContext.context";
 
 import Header from "./components/header/header.component";
 import Profile from "./pages/profile/profile.pages";
 import "./App.css";
 
 const App = () => (
-  <Router>
-    <div className="App">
-      <Switch>
-        <Route path="/" component={Login} exact />
-        <Route path="/signup" component={SignUp} exact />
-        {/* <Route path="/test" component={TestPage} exact /> */}
-        <Route path="/" component={Header} />
-      </Switch>
-      <Route path="/quick-match" />
-      <Route path="/combo-match" />
-      <Route path="/friends" />
-      <Route path="/inbox" />
-      <Route path="/profile" component={Profile} />
-    </div>
-  </Router>
+  <UserProvider>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" component={Login} exact />
+          <Route path="/signup" component={SignUp} exact />
+          {/* <Route path="/test" component={TestPage} exact /> */}
+          <Route path="/" component={Header} />
+        </Switch>
+        <Route path="/quick-match" />
+        <Route path="/combo-match" />
+        <Route path="/friends" />
+        <Route path="/inbox" />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </Router>
+  </UserProvider>
 );
 
 export default App;
