e10c9ea068fb134f6a0e2253237c2a72aac4bf4b
Felix Halim <felix.halim@outlook.com>
2020-07-06 17:41:37 +0800

    feat: information modal pop-up

--- src/components/user-card/user-card.component.jsx
+++ src/components/user-card/user-card.component.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardActionArea,
@@ -9,6 +9,10 @@ import {
   IconButton,
   Grid,
   Tooltip,
+  Modal,
+  Backdrop,
+  Fade,
+  Button,
 } from "@material-ui/core";
 import {
   Timeline,
@@ -18,102 +22,213 @@ import {
   Book,
   Home,
 } from "@material-ui/icons";
+import "../particulars/cards.styles.css";
 
 const UserCard = (props) => {
   const { residence, nationality, major, career } = props.userData;
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <Card
-      style={{
-        borderRadius: "20px",
-        color: "white",
-        backgroundColor: "#372f6c",
-      }}
-    >
-      <CardActionArea>
-        <CardMedia
-          style={{
-            height: 140,
-            backgroundColor: "#3f3772",
-          }}
-          image="/"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography align="center" gutterBottom variant="h5" component="h2">
-            {props.name}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Grid container alignItems="center">
-          <Grid container align="center" item xs={4}>
-            <Grid item xs={12}>
-              <Tooltip title={props.career || "Unknown"} arrow placement="top">
-                <IconButton disableRipple>
-                  <Timeline
-                    htmlColor={career === props.career ? "white" : ""}
-                  />
-                </IconButton>
-              </Tooltip>
+    <>
+      <Card
+        style={{
+          maxWidth: 345,
+          borderRadius: "20px",
+          color: "white",
+          backgroundColor: "#372f6c",
+        }}
+      >
+        <CardActionArea
+          href={props.isFriend && `chat/${props.username}`}
+          onClick={!props.isFriend && handleOpen}
+        >
+          <CardMedia
+            style={{
+              height: 0,
+              paddingTop: "56.25%",
+              backgroundColor: "#3f3772",
+            }}
+            image={props.avatar}
+            title={props.name}
+          />
+          <CardContent>
+            <Typography align="center" variant="h5">
+              {props.name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Grid container alignItems="center">
+            <Grid container align="center" item xs={4}>
+              <Grid item xs={12}>
+                <Tooltip
+                  title={props.career || "Unknown"}
+                  arrow
+                  placement="top"
+                >
+                  <IconButton disableRipple>
+                    <Timeline
+                      htmlColor={career === props.career ? "white" : ""}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item xs={12}>
+                <Tooltip
+                  title={props.nationality || "Unknown"}
+                  arrow
+                  placement="top"
+                >
+                  <IconButton disableRipple>
+                    <Language
+                      htmlColor={
+                        nationality === props.nationality ? "white" : ""
+                      }
+                    />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Tooltip
-                title={props.nationality || "Unknown"}
-                arrow
-                placement="top"
-              >
-                <IconButton disableRipple>
-                  <Language
-                    htmlColor={nationality === props.nationality ? "white" : ""}
-                  />
-                </IconButton>
-              </Tooltip>
+            <Grid
+              item
+              align="center"
+              xs={4}
+              style={{
+                backgroundColor: "#8b66e0",
+                borderRadius: "20px",
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              }}
+            >
+              <IconButton>
+                {props.isFriend ? (
+                  <Chat fontSize="large" />
+                ) : (
+                  <PersonAdd fontSize="large" />
+                )}
+              </IconButton>
+            </Grid>
+            <Grid container align="center" item xs={4}>
+              <Grid item xs={12}>
+                <Tooltip
+                  title={props.residence || "Unknown"}
+                  arrow
+                  placement="top"
+                >
+                  <IconButton color="custom" disableRipple>
+                    <Home
+                      htmlColor={residence === props.residence ? "white" : ""}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+              <Grid item xs={12}>
+                <Tooltip title={props.major || "Unknown"} arrow placement="top">
+                  <IconButton disableRipple>
+                    <Book htmlColor={major === props.major ? "white" : ""} />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid
-            item
-            align="center"
-            xs={4}
+        </CardActions>
+      </Card>
+
+      <Modal
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <Card
             style={{
-              backgroundColor: "#8b66e0",
+              maxWidth: 345,
               borderRadius: "20px",
-              boxShadow:
-                "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+              color: "white",
+              backgroundColor: "#372f6c",
+              outline: 0,
             }}
           >
-            <IconButton>
-              {props.isFriend ? (
-                <Chat fontSize="large" />
-              ) : (
-                <PersonAdd fontSize="large" />
-              )}
-            </IconButton>
-          </Grid>
-          <Grid container align="center" item xs={4}>
-            <Grid item xs={12}>
-              <Tooltip
-                title={props.residence || "Unknown"}
-                arrow
-                placement="top"
+            <CardMedia
+              style={{
+                height: 0,
+                paddingTop: "56.25%",
+                backgroundColor: "#3f3772",
+              }}
+              image={props.avatar}
+              title={props.name}
+            />
+            <CardContent>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  <Typography align="center" gutterBottom variant="h5">
+                    {props.name}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Timeline fontSize="inherit" style={{ marginRight: "1vw" }} />
+                  <Typography align="left" variant="subtitle">
+                    {props.career || "Unknown"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Book fontSize="inherit" style={{ marginRight: "1vw" }} />
+                  <Typography align="left" variant="subtitle">
+                    {props.major || "Unknown"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Language fontSize="inherit" style={{ marginRight: "1vw" }} />
+                  <Typography align="left" variant="subtitle">
+                    {props.nationality || "Unknown"}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Home fontSize="inherit" style={{ marginRight: "1vw" }} />
+                  <Typography align="left" variant="subtitle">
+                    {props.residence || "Unknown"}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+            <CardActions
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: "1vh",
+              }}
+              disableSpacing
+            >
+              <Button
+                variant="contained"
+                size="medium"
+                color="secondary"
+                startIcon={props.isFriend ? <Chat /> : <PersonAdd />}
               >
-                <IconButton color="custom" disableRipple>
-                  <Home
-                    htmlColor={residence === props.residence ? "white" : ""}
-                  />
-                </IconButton>
-              </Tooltip>
-            </Grid>
-            <Grid item xs={12}>
-              <Tooltip title={props.major || "Unknown"} arrow placement="top">
-                <IconButton disableRipple>
-                  <Book htmlColor={major === props.major ? "white" : ""} />
-                </IconButton>
-              </Tooltip>
-            </Grid>
-          </Grid>
-        </Grid>
-      </CardActions>
-    </Card>
+                Add as Friend
+              </Button>
+            </CardActions>
+          </Card>
+        </Fade>
+      </Modal>
+    </>
   );
 };
 
